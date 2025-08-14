936103a500be928210a7526e28cfbe9993d3b76b
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-08-14 17:27:25 +0300

    Add JWT authentication

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -149,7 +150,16 @@ MEDIA_URL = "/media/"
 
 # REST settings
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5,
-    "MAX_PAGE_SIZE": 50
+    "MAX_PAGE_SIZE": 50,
+}
+
+# JWT settings
+SIMPLE_JWT = {
+   "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+   "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
