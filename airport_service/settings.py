2a686b5c1452c70a64a268e6beb2438a789566ba
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-08-14 18:09:15 +0300

    Add throttling

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -153,6 +153,14 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    },
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 5,
     "MAX_PAGE_SIZE": 50,
