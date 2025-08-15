b71938f6cc10cdcff50545f8e48cea6889a7f111
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-08-15 16:22:52 +0300

    Add api/doc/ urls

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from airport_service import settings
 
@@ -25,5 +26,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api/airport/", include("airport.urls", namespace="airport")),
-    path("api/user/", include("user.urls", namespace="user"))
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
